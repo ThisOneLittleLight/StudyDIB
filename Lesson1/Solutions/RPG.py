f86b807d91970e9bef05bb81cdb8f3e1f97f5787
Philip Krzoska <138976529+ThisOneLittleLight@users.noreply.github.com>
2024-02-02 18:35:16 +0100

    Worked on the task description

--- Lesson1/Solutions/RPG.py
+++ Lesson1/Solutions/RPG.py
@@ -15,10 +15,13 @@ class Character:
     
     #This function takes a character that we want to attack and calls their hit function
     def attack(self, char):
-        char.hit(self.damage)
+        char.hit(self.damage)   #Call the chars hit function and give it the classes damage
+
+        #This is just to organize the console
         print(self.name, " attacked ", char.name)
         print("")
     
+    #Print the stats of the character
     def print_stats(self):
         print(self.name,"'s stats are:")
         print("HEALTH: ", self.health, " DAMAGE: ", self.damage)
@@ -27,17 +30,25 @@ class Character:
         
 
 class Player(Character):
+    inventory = []
     def __init__(self, name, health, damage):
         super().__init__(name, health, damage)
 
 class Enemy(Character):
-    def __init__(self, name, health, damage):
+    battlecry = ""
+    def __init__(self, name, health, damage, battlecry):
         super().__init__(name, health, damage)
+        self.battlecry = battlecry
+    
+    def cry_battlecry(self):
+        print(self.name, " screams: ", self.battlecry)
+        print("")
 
 
 player = Player("Player" ,10, 4)
-enemy = Enemy("Enemy", 4, 1)
+enemy = Enemy("Enemy", 4, 1, "REEEEEEEEEEEE")
 
+enemy.cry_battlecry()
 enemy.print_stats()
 
 player.attack(enemy)
---
+++ Lesson1/Tasks/TASK_RPG.py
@@ -0,0 +1,37 @@
+#NR 1)
+#Create a class named "Character" that has the following attributes
+#name
+#health
+#damage
+#
+#And set those in the constructor
+
+#NR 2)
+#Create 2 functions inside the Character class
+#
+#The first is a hit function that takes the amount of damage and substracts it from the health
+#
+#The second is an attack function that takes the character to attack and calls their hit function
+#
+#The third is to print its stats to the console (health and damage)
+
+#NR 3)
+#Create a player class that inherits from Character
+#
+#The player has an inventory attribute which is a list containing the items (With items I just mean Strings that depict what would be real Item names in a real game)
+#
+#Now add a function that adds a item to the inventory. It should take a string as an agrument to add to the list
+
+#NR 4)
+#Create an Enemy class that has a battle cry varible
+#
+#Set the battle cry variable in the enemies constructor (Make sure everything else is still set from the Character class HINT: use super.)
+#
+#Create a function for the enemy to shout out its battle cry to the console
+
+#NR 5)
+#Now test out your program in the following way:
+#   -The Enemy shouts its battlecry
+#   -The Enemies stats are printed
+#   -The Player attacks the Enemy
+#   -The Enemies stats are printed again
